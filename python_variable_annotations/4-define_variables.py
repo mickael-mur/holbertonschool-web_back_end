3647c61ccc5877fb2bef66685684f833db3d885a
Krapaud <mur.mickael@gmail.com>
2025-11-25 18:00:07 +0100

    Define and annotate variables with initial values

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -2,7 +2,7 @@
 """Define and annotate variables"""
 
 
-a: int
-pi: float
-i_understand_annotations: bool
-school: str
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
