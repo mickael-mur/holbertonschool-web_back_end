41d710bc54e63c4e720e1fca7331e3dcea804926
Krapaud <mur.mickael@gmail.com>
2025-11-26 10:32:28 +0100

    Fix variable name typo in wait_n function for consistency

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -7,8 +7,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> list[float]:
     """Spawn wait_random n times with the specified max_delay
     and return the list of all delays in ascending order."""
-    co_routine = [wait_random(max_delay) for _ in range(n)]
+    coroutine = [wait_random(max_delay) for _ in range(n)]
     delays: list[float] = []
-    for coro in asyncio.as_completed(co_routine):
+    for coro in asyncio.as_completed(coroutine):
         delays.append(await coro)
     return delays
