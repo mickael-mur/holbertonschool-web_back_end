2ea80b4a851bef742f84fd85791ae1b5feea4589
Krapaud <mur.mickael@gmail.com>
2025-12-03 09:59:43 +0100

    Add main scripts for MongoDB operations and implement school insertion

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -7,12 +7,13 @@ Module for inserting a document in a MongoDB collection
 def insert_school(mongo_collection, **kwargs):
     """
     Inserts a new document in a collection based on kwargs
-    
+
     Args:
         mongo_collection: pymongo collection object
         **kwargs: key-value pairs to insert as document
-    
+
     Returns:
         The new _id of the inserted document
     """
-    pass
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
