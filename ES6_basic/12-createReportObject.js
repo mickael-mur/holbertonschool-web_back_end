763640f9737323ebc65de1b87c5751baa1ee2312
Krapaud <mur.mickael@gmail.com>
2025-12-08 15:23:21 +0100

    Refactor createReportObject to improve structure and readability

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,3 +1,10 @@
-export default function createReportObject(employeesList) {
+import createEmployeesObject from './11-createEmployeesObject.js';
 
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments(employees) {
+        return Object.keys(employees).length
+    }
+  };
 }
