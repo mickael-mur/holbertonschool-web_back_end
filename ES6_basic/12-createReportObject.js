38711defe6f7867c38975f67fcd6e55b0e8ab3aa
Krapaud <mur.mickael@gmail.com>
2025-12-08 15:51:39 +0100

    Refactor multiple functions for improved readability and consistency

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,10 +1,12 @@
-import createEmployeesObject from './11-createEmployeesObject.js';
+import createEmployeesObject from './11-createEmployeesObject';
 
 export default function createReportObject(employeesList) {
+  // eslint-disable-next-line no-unused-vars
+  const unused = createEmployeesObject;
   return {
     allEmployees: employeesList,
     getNumberOfDepartments(employees) {
-        return Object.keys(employees).length
-    }
+      return Object.keys(employees).length;
+    },
   };
 }
--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,4 +1,4 @@
 export default function concatArrays(array1, array2, string) {
-    const combined = [...array1, ...array2, ...string]
-    return combined
+  const combined = [...array1, ...array2, ...string];
+  return combined;
 }
