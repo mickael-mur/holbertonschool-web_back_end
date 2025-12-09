612dd6b7a771d06542e5ba9f5db6bcc1b73f11cc
Krapaud <mur.mickael@gmail.com>
2025-12-09 09:08:55 +0100

    Refactor getBudgetObject to improve clarity by using a budget object

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,4 +1,10 @@
 export default function getBudgetObject(income, gdp, capita) {
-  return { income, gdp, capita };
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
 }
 
