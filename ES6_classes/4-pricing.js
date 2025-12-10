ebf0852b0955627689a33ca5f95233a1c69734fc
Krapaud <mur.mickael@gmail.com>
2025-12-10 11:25:09 +0100

    Implement Pricing class with constructor, getters, setters, and displayFullPrice method

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,7 +1,34 @@
-// TODO: Import Currency from 3-currency.js
-// TODO: Implement Pricing class
-// Constructor: amount (Number), currency (Currency)
-// - Store in underscore versions: _amount, _currency
-// - Implement getter and setter for each attribute
-// - Implement displayFullPrice method: "amount currency_name (currency_code)"
-// - Implement static method convertPrice(amount, conversionRate): return amount * conversionRate
+import Currency from './3-currency.js';
+
+export default class Pricing {
+    constructor(amount, currency) {
+        this._amount = amount
+        this._currency = currency
+    }
+    //getter
+    get amount() {
+        return this._amount;
+    }
+
+    get currency() {
+        return this._currency;
+    }
+    //setter with type verification
+    set amount(value) {
+        if (typeof value !== "number" || Number.isNaN(value)) {
+            throw new TypeError("Amount must be an integer")
+        }
+        this._amount = value;
+    }
+
+    set currency(value) {
+        if (typeof value !== "object") {
+            throw new TypeError("Currency must be an object")
+        }
+        this._currency = value;
+    }
+
+    displayFullPrice() {
+        return `${this._amount} ${this._currency.displayFullCurrency()}`
+    } 
+}
