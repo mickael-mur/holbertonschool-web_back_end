c3e2d6a529ce20db8f0b6a251864540d9a6fee29
Krapaud <mur.mickael@gmail.com>
2025-12-10 18:03:59 +0100

    Implement HolbertonClass with constructor, valueOf, and toString methods

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -1,6 +1,14 @@
-// TODO: Implement HolbertonClass class
-// Constructor: size (Number), location (String)
-// - Store in _size, _location
-// - When cast to Number, return size
-// - When cast to String, return location
-// Hint: Implement valueOf() and toString() methods
+export default class HolbertonClass {
+    constructor(size, location) {
+        this._size = size
+        this._location = location
+    }
+
+    valueOf() {
+        return this._size;
+    }
+
+    toString() {
+        return this._location;
+    }
+}
