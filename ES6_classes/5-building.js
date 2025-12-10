66e4f30c3b877fe10547ca8931102ea9648e40f6
Krapaud <mur.mickael@gmail.com>
2025-12-10 17:30:57 +0100

    Implement Building class as an abstract class with sqft property and validation

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,6 +1,23 @@
-// TODO: Implement Building class (abstract class)
-// Constructor: sqft (Number)
-// - Store in _sqft
-// - Implement getter for sqft
-// - Check if extending class implements evacuationWarningMessage method
-// - If not, throw error: "Class extending Building must override evacuationWarningMessage"
+export default class Building {
+    constructor(sqft) {
+        if (this.constructor !== Building && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+            throw new Error('Class extending Building must override evacuationWarningMessage');
+        }
+        this._sqft = sqft;
+    }
+    //getters
+    get sqft() {
+        return this._sqft;
+    }
+
+    set sqft(value) {
+        if (typeof value !== "number" || Number.isNaN(value)) {
+            throw new TypeError('sqft must be an integer')
+        }
+        this._sqft = value;
+    }
+
+    evacuationWarningMessage() {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+}
