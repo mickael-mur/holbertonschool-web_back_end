1ddc507eae434717862fd44bd669c38f3f259a89
Krapaud <mur.mickael@gmail.com>
2025-12-10 10:21:51 +0100

    Implement ClassRoom class and initializeRooms function to create classroom instances

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,3 +1,13 @@
-// TODO: Import ClassRoom from 0-classroom.js
-// TODO: Implement initializeRooms function
-// - Return array of 3 ClassRoom objects with sizes: 19, 20, 34
+export class ClassRoom {
+    constructor(maxStudentsSize) {
+        this._maxStudentsSize = maxStudentsSize;
+    }
+}
+
+function initializeRooms() {
+        const sizes = [19, 20, 34]
+        const newArr = sizes.map(size => new ClassRoom(size))
+        return newArr
+}
+
+export default initializeRooms;
