927522250e40a70d09fae973ee37071ff518df17
Krapaud <mur.mickael@gmail.com>
2025-12-10 10:44:40 +0100

    Implement HolbertonCourse class with constructor, getters, and setters including type verification

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,5 +1,45 @@
-// TODO: Implement HolbertonCourse class
-// Constructor: name (String), length (Number), students (Array of Strings)
-// - Store in underscore versions: _name, _length, _students
-// - Verify type of each attribute during creation
-// - Implement getter and setter for each attribute
+export default class HolbertonCourse {
+    constructor(name, length, students) {
+        this._name = name
+        this._length = length
+        this._students = students
+    }
+    //getters
+    get name() {
+        return this._name;
+    }
+
+    get length() {
+        return this._length;
+    }
+
+    get students() {
+        return this._students;
+    }
+    //setters with type verification
+    set name(value) {
+        if (typeof value !== "string") {
+            throw new TypeError("Name must be a string");
+        }
+        this._name = value
+    }
+
+    set length(value) {
+        if (typeof value !== "number" || Number.isNaN(value)) {
+            throw new TypeError("Length must be an integer")
+        }
+        this._length = value
+    }
+
+    set students(value) {
+        if (!Array.isArray(value)) {
+            throw new TypeError("Students need to be an array");
+        }
+        for (const student of value) {
+            if (typeof student !== "string") {
+                throw new TypeError("Student need to be a string");
+            }
+        }
+        this._students = value
+    }
+}
