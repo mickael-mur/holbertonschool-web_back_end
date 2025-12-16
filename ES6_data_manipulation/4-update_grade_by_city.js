bae9e8218f45f7f81d699649929685e5b050b89c
Krapaud <mur.mickael@gmail.com>
2025-12-16 09:45:54 +0100

    Remove TODO comments from updateStudentGradeByCity function

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,3 @@
-// TODO: Create a function updateStudentGradeByCity
-// Must use filter and map combined
-// If student doesn't have grade, set it to 'N/A'
 export default function updateStudentGradeByCity(students, city, newGrades) {
     return students
         .filter((student) => student.location === city)
