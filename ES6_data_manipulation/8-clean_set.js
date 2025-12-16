b6833baefef86c3085ab5af9b4a25d6a27c85def
Krapaud <mur.mickael@gmail.com>
2025-12-16 11:29:44 +0100

    Implement cleanSet function to return a string of set values starting with a specified prefix

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,16 @@
 // TODO: Create a function cleanSet
 // Returns a string of set values that start with startString
 // Values separated by '-', only append the rest of the string after startString
+export default function cleanSet(set, startString) {
+    if (startString === null || startString === '') {
+        return ''
+    }
+    const result = []
+    for (const value of set) {
+       if (value.startsWith(startString)) {
+        value.slice(startString.length)
+        result.push(value.slice(startString.length))
+       }
+    }
+    return result.join('-');
+}
