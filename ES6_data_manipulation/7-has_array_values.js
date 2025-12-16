1ccbec81c87f0e93fa7da36e9ca73bded60ebb0a
Krapaud <mur.mickael@gmail.com>
2025-12-16 10:14:54 +0100

    Implement hasValuesFromArray function to check if all elements in an array exist in a set

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,2 +1,5 @@
 // TODO: Create a function hasValuesFromArray
 // Returns true if all elements in array exist in the set
+export default function hasValuesFromArray(set, array) {
+    return array.every((element) => set.has(element));
+}
