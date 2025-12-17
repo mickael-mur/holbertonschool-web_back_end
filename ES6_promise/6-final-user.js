740f6f0282556ab48258222b997929a3a22fb4ca
Krapaud <mur.mickael@gmail.com>
2025-12-17 14:22:54 +0100

    Refactor handleProfileSignup function to eliminate intermediate promises array

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,12 +2,10 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [
+  return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ];
-
-  return Promise.allSettled(promises)
+  ])
   .then((results) => {
     return results.map((result) => {
       if (result.status === 'rejected') {
