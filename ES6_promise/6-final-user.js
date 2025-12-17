d529f3b2dd8f3d156e70291adaf13eb4b8232ff1
Krapaud <mur.mickael@gmail.com>
2025-12-17 14:17:35 +0100

    Make handleProfileSignup function asynchronous to improve promise handling

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,7 +2,7 @@ import uploadPhoto from './5-photo-reject.js';
 import signUpUser from './4-user-promise.js';
 
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
