5cff791ae45513ffdd491602138ec7f9d0904276
Krapaud <mur.mickael@gmail.com>
2025-12-17 14:22:17 +0100

    Refactor handleProfileSignup function to store promises in a variable for clarity

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,10 +2,12 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
+  const promises = [
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ])
+  ];
+
+  return Promise.allSettled(promises)
   .then((results) => {
     return results.map((result) => {
       if (result.status === 'rejected') {
