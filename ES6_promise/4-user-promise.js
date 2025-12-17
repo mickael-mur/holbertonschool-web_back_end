1e03b09b5bda7b04ef32519e80cfee6b10b2e7a7
Krapaud <mur.mickael@gmail.com>
2025-12-17 14:01:45 +0100

    Remove type validation from signUpUser function to simplify promise resolution

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,10 +1,8 @@
 export default function signUpUser(firstName, lastName) {
   return new Promise((resolve, reject) => {
-    if (typeof firstName === 'string' && typeof lastName === 'string') {
       resolve({
         firstName,
         lastName,
       });
-    }
-  });
+    });
 }
