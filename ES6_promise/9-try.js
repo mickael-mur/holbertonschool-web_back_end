8c5dd162afeab8a5305d23655e1dadfd44abadf4
Krapaud <mur.mickael@gmail.com>
2025-12-17 14:44:49 +0100

    Implement guardrail function to handle execution and error processing of a math function

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,3 +1,14 @@
 export default function guardrail(mathFunction) {
-  // TODO: Implement
+  const queue = [];
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  }
+  catch (error) {
+    queue.push(error);
+  }
+  finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
 }
