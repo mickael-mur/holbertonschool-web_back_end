146b6f9280fada650c127a9b9c456f1f23860595
Krapaud <mur.mickael@gmail.com>
2025-12-17 13:47:56 +0100

    Implement uploadPhoto function to reject promises with an error message

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,3 +1,5 @@
 export default function uploadPhoto(filename) {
-  // TODO: Implement
-}
+  return new Promise((resolve, reject) => {
+      reject(new Error(`${filename} cannot be processed`));
+    }
+  )};
