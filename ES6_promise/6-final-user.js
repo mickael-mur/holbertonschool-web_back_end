715d71c13e3f7468197e8e70e48fb75e8dba4e50
Krapaud <mur.mickael@gmail.com>
2025-12-17 13:57:33 +0100

    Implement handleProfileSignup function to manage user signup and photo upload

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,3 +1,10 @@
+import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise.js';
+
+
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  // TODO: Implement
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]);
 }
