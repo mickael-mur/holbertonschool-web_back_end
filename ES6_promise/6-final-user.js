3547346f5d0620a277347d434910c1bf04350fd3
Krapaud <mur.mickael@gmail.com>
2025-12-17 14:18:58 +0100

    Reorder import statements and remove async from handleProfileSignup function

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,8 +1,7 @@
-import uploadPhoto from './5-photo-reject.js';
 import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
-
-export default async function handleProfileSignup(firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
